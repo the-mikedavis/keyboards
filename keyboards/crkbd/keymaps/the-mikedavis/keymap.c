0211661c057c49c7e3b73d26cf8564d8564dce73
Michael Davis <mcarsondavis@gmail.com>
2022-07-06 10:51:30 -0500

    crkbd: switch minus and underscore

--- keyboards/crkbd/keymaps/the-mikedavis/keymap.c
+++ keyboards/crkbd/keymaps/the-mikedavis/keymap.c
@@ -31,12 +31,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MD_CTLQT CTL_T(KC_QUOT)
 #define MD_GUIQ GUI_T(KC_Q)
 
+enum custom_keycodes {
+  MD_REVMINS = SAFE_RANGE,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_PDVORAK] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, KC_SCLN, KC_COMM,  KC_DOT,    KC_P,    KC_Y,                         KC_F,    KC_G,    KC_C,    KC_R,   KC_L, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    MD_DIRESC,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                         KC_D,    KC_H,    KC_T,    KC_N,   KC_S, KC_MINS,
+    MD_DIRESC,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                         KC_D,    KC_H,    KC_T,    KC_N,   KC_S, MD_REVMINS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LSFT, MD_CTLQT, MD_GUIQ,    KC_J,    KC_K,    KC_X,                         KC_B,    KC_M,    KC_W,    KC_V,   KC_Z, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -181,6 +185,17 @@ bool oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     set_keylog(keycode, record);
+
+    // switch minus and underscore
+    if (keycode == MD_REVMINS) {
+      if (get_mods() & MOD_MASK_SHIFT) {
+        unregister_code(KC_LSFT);
+        SEND_STRING("-");
+        register_code(KC_LSFT);
+      } else {
+        SEND_STRING("_");
+      }
+    }
   }
   return true;
 }
